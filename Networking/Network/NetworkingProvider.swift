2a5b8ea8bd30353846554c4d8010b44d6c8f22c4
davidsantander9@gmail.com <davidsantander9@gmail.com>
2023-10-22 21:39:36 -0600

    add Alamofire

---
+++ Networking/Network/NetworkingProvider.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingProvider.swift
+//  Networking
+//
+//  Created by David C Santander on 22/10/23.
+//
+
+import Foundation
+import Alamofire
+
+final class NetworkingProvider {
+    
+    static let shared = NetworkingProvider()
+    
+    
+    
+}
